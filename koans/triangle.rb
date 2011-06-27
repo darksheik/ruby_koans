bb97386d0c711b99fa341d2c91994fccb28da071
Don Pflaster <dpflaster@gmail.com>
2011-06-26 22:47:51 -0500

    Further along

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,31 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+   equal_sides = 0
+   
+   if (a <= 0 || b <= 0 || c <= 0)
+     raise TriangleError.new("You have sides that are zero or less, dummy.")
+   end
+   if (a+b <= c) || (a+c <= b) || (b+c <= a)
+     raise TriangleError.new("Two sides must add up to more than the third.")
+   end
+   
+   if (a == b) && (b == c)
+     equal_sides = 3
+   end
+   if ((b == c) && (a != b)) || ((a == c) && (a != b)) || ((a == b) && (b != c))
+     equal_sides = 2
+   end
+   
+   #puts a.to_s + " " + b.to_s + " " + c.to_s + " es: " + equal_sides.to_s
+   if equal_sides == 3
+     :equilateral
+   elsif equal_sides == 2
+     :isosceles
+   else
+     :scalene
+   end
+
 end
 
 # Error class used in part 2.  No need to change this code.
